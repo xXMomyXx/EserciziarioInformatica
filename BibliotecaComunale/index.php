1ae3c85fbba12989bd18dbd940bba69742fe2d48
Mohamed Guezam <150335339+xXMomyXx@users.noreply.github.com>
2026-02-18 22:26:41 +0100

    Aggiunta pagina principale Biblioteca Comunale

---
+++ BibliotecaComunale/index.php
@@ -0,0 +1,23 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>BIBLIOTECA COMUNALE</title>
+</head>
+<body>
+    <div class = "header">
+        <img src="https://cdn-icons-png.flaticon.com/512/2784/2784539.png" alt="Icona Libreria" class="icon">
+        <h1 class="title">BENVENUTO ALLA BIBLIOTECA COMUNALE!</h1>
+        <h1 id = "time"></h1>
+        <h3 id = "date"></h3>
+        <button id = "initialButton">ISCRIVITI</button>
+    </div>
+</body>
+<script src = "script.js"></script>
+</html>
