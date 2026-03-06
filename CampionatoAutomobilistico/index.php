64e25cff911911d49545302910c57272fc7b522b
Mohamed Guezam <150335339+xXMomyXx@users.noreply.github.com>
2026-03-06 12:48:21 +0100

    Create index.php for CampionatoAutomobilistico
    Struttura HTML iniziale per CampionatoAutomobilistico

---
+++ CampionatoAutomobilistico/index.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel = "stylesheet" href = "style.css">
+    <title>Campionato automobilistico</title>
+</head>
+<body>
+    <form method="post" target="elab.php">
+        <div class="header">
+            <label for="nameBox">Nome </label><input id="nameBox" type="text" name="nome" required>
+            <label for="surnameBox">Cognome </label><input id="surnameBox" type="text" name="cognome" required>
+        </div>
+    </form>
+</body>
+</html>
