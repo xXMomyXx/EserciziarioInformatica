6f5e7c6918bc90d388fb02604b4ed29f7768312e
Mohamed Guezam <150335339+xXMomyXx@users.noreply.github.com>
2026-03-07 10:45:54 +0100

    Update labels and stylesheet path in index.php

--- CampionatoAutomobilistico/index.php
+++ CampionatoAutomobilistico/index.php
@@ -9,14 +9,16 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel = "stylesheet" href = "style.css">
+    <link rel = "stylesheet" href = "css/style.css">
     <title>Campionato automobilistico</title>
 </head>
 <body>
     <form method="post" target="elab.php">
         <div class="header">
-            <label for="nameBox">Nome </label><input id="nameBox" type="text" name="nome" required>
-            <label for="surnameBox">Cognome </label><input id="surnameBox" type="text" name="cognome" required>
+            <label for="nameBox">NOME </label><input id="nameBox" type="text" name="nome" required>
+            <label for="surnameBox">COGNOME </label><input id="surnameBox" type="text" name="cognome" required>
+            <label for="nationalityBox">NAZIONALITA'  </label><input id="nationalityBox" type="text" name="cognome" required>
+            <label for="phoneNumberBox">CELLULARE </label><input id="phoneNumberBox" type="tel" name="cognome" required>
         </div>
     </form>
 </body>
