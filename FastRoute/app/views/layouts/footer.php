c5b85362ab668347d5009f65558c8c7ad38ecf29
Mohamed Guezam <150335339+xXMomyXx@users.noreply.github.com>
2026-05-25 15:06:41 +0200

    Add footer with copyright information

---
+++ FastRoute/app/views/layouts/footer.php
@@ -0,0 +1,6 @@
+</div>
+<footer style="text-align: center; margin-top: 20px; padding: 10px; font-size: 0.8em; color: #555; border-top: 1px solid #ddd;">
+    Copyright IIS Viola Marchesini 2026
+</footer>
+</body>
+</html>
