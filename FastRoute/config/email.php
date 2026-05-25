fcd249eec4c8e7b299f4ef054348371f09d6f0c1
Mohamed Guezam <150335339+xXMomyXx@users.noreply.github.com>
2026-05-25 15:11:23 +0200

    Add files via upload

---
+++ FastRoute/config/email.php
@@ -0,0 +1,8 @@
+<?php
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_PORT', 587);
+define('SMTP_USER', 'mohamed.guezam@iisviolamarchesini.edu.it');
+define('SMTP_PASS', 'dvbn spmx mewy favi');
+define('SMTP_SECURE', 'tls');
+define('MAIL_FROM', 'mohamed.guezam@iisviolamarchesini.edu.it');
+define('MAIL_FROM_NAME', 'FastRoute Corriere');
---
+++ FastRoute/config/requireconfig.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/databaseConfig.php';
+require_once __DIR__ . '/appconfig.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/email.php';
+?>
